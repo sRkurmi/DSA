9e89bbf60082a7e40c06994c7f7d48a86aaa1dd9
shrikant <shrikantvakna@gmail.com>
2026-03-10 18:50:58 +0530

    Implementation of the max func to find maximum

--- Array_ADT/operations_on_arr/p1.cpp
+++ Array_ADT/operations_on_arr/p1.cpp
@@ -19,6 +19,8 @@ void display(struct Array arr)
 	for(int i = 0; i < arr.length; i++)
 		cout<<arr.A[i]<<" ";
 }
+
+/*given below is the get function code */
 int Get(struct Array arr,int index)
 {
 	/*first we will check whether the index is valid or not */
@@ -26,6 +28,8 @@ int Get(struct Array arr,int index)
 		return arr.A[index];// here we returning the element at that perticular index
 	return -1;
 }
+
+/* given below is the set function code */
 int set(struct Array *arr,int index, int x)
 {
 	/* so this function is taking 3 parameter 1st is array , 2nd is index, 3rd is new value */
@@ -35,14 +39,27 @@ int set(struct Array *arr,int index, int x)
 		arr->A[index] = x;
 		return arr->A[index];
 }
+
+/* Given below is the max function code to find the maximum value */
+int max(struct Array arr)
+{
+	int max = arr.A[0];
+	for(int i = 0; i < arr.length; i++)
+		{
+			if(max < arr.A[i])
+				max = arr.A[i];
+		}
+	return max;
+}
 int main()
 {
-	struct Array arr = {{2,3,4,5,6},10,5};
+	struct Array arr = {{-2,-3,-4,-5,-6},10,5};
 	cout<<"Given below is the array we kept"<<endl;
 	display(arr);
 	cout<<endl;
-	cout<<"Get fun call = "<<Get(arr,2)<<endl;// this is the function call for get function
-	cout<<"the new value by set func = "<<set(&arr,2,1000)<<endl;
+	cout<<"Get fun call value at index 2 = "<<Get(arr,2)<<endl;// this is the function call for get function
+	cout<<"the new value by set func = "<<set(&arr,0,-1)<<endl;
+	cout<<"The maximum value inside the array is = "<<max(arr)<<endl;
 	cout<<"The changed Array is given below"<<endl;
 	display(arr);
 	cout<<endl;
