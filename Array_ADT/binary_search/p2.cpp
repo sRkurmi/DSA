5856bd78aa580483dac50334ec1c6a8a1a0c49bf
shrikant <shrikantvakna@gmail.com>
2026-03-10 16:30:06 +0530

    Implementation of binary search by recursive method

---
+++ Array_ADT/binary_search/p2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+/* In this example we will see the implementation of the binary search by recursive method */
+
+struct Array 
+{
+	int A[10];
+	
+	int size;
+
+	int length;
+};
+void display(struct Array arr)
+{
+	for(int i = 0; i < arr.length; i++)
+		cout<<arr.A[i]<<" ";
+}
+
+int RBinsearch(int a[],int l,int h,int key)
+{
+	int mid;
+	if(l <= h)
+	{
+	    mid = (l+h)/2;
+
+		if(key == a[mid])
+			return mid;
+
+		else if(key < a[mid] )
+			return RBinsearch(a,l,mid-1,key);
+		
+		else
+			return RBinsearch(a,mid+1,h,key);
+	}
+	return -1;
+}
+int main()
+{
+	struct Array arr = {{2,3,4,5,6},10,5};// this initialization of the array
+	
+	cout<<RBinsearch(arr.A,0,arr.length-1,3)<<endl;
+	display(arr);
+	cout<<endl;
+	return 0;
+}
