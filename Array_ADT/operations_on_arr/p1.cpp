e06c2c6debe3d705e8b1281a6983902b09328f1a
shrikant <shrikantvakna@gmail.com>
2026-03-10 18:34:37 +0530

    Implementation of the set function

--- Array_ADT/operations_on_arr/p1.cpp
+++ Array_ADT/operations_on_arr/p1.cpp
@@ -26,11 +26,24 @@ int Get(struct Array arr,int index)
 		return arr.A[index];// here we returning the element at that perticular index
 	return -1;
 }
+int set(struct Array *arr,int index, int x)
+{
+	/* so this function is taking 3 parameter 1st is array , 2nd is index, 3rd is new value */
+
+	/* first we check we the index whether the index is valid or not */
+	if(index >= 0 && index < arr->length)
+		arr->A[index] = x;
+		return arr->A[index];
+}
 int main()
 {
 	struct Array arr = {{2,3,4,5,6},10,5};
-	
-	cout<<Get(arr,2)<<endl;;// this is the function call
+	cout<<"Given below is the array we kept"<<endl;
+	display(arr);
+	cout<<endl;
+	cout<<"Get fun call = "<<Get(arr,2)<<endl;// this is the function call for get function
+	cout<<"the new value by set func = "<<set(&arr,2,1000)<<endl;
+	cout<<"The changed Array is given below"<<endl;
 	display(arr);
 	cout<<endl;
 	return 0;
