7270e90afc0f9cfc6c8bae73a255066a9c0d8043
shrikant <shrikantvakna@gmail.com>
2026-04-02 11:21:31 +0530

    simple function to find out the lenght of the string

---
+++ string/length_of_string/p1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char s[] = "welcome";
+
+	int i;
+
+	for(i = 0; s[i]!= '\0'; i++)
+	{
+
+	}
+	cout<<"Length = "<<i<<endl;
+
+return 0;	
+}
