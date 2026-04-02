a823b7ade4360ceadba46c9f0ad55e2be654315d
shrikant <shrikantvakna@gmail.com>
2026-04-02 11:45:51 +0530

    simple program to change upper case to lower case

---
+++ string/changing_case/p1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char s[] = "WELCOME";
+	cout<<"Inistially the string is = "<<s<<endl;	
+	for(int i = 0;s[i]!=0; i++)
+	{
+		if(s[i]>='A'&& s[i]<='Z')
+			s[i]+=32;	
+	}
+	cout<<s<<endl;
+return 0;
+}
