0f19d99c429d790f806f7bc606f0174a7de1531b
shrikant <shrikantvakna@gmail.com>
2026-04-02 12:24:51 +0530

    toggling the case

---
+++ string/changing_case/p3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char A[] = "WeLCoMe";
+	cout<<A<<endl;
+	for(int i = 0; A[i]!=0;i++)
+	{
+		if(A[i] >= 65 && A[i] <= 90)
+			A[i]+=32;
+		else if(A[i] >= 97 && A[i] <= 122)
+			A[i]-=32;
+	}
+	cout<<A<<endl;
+	
+return 0;
+}
