77b2ac3513a401c4e982f17a2bad7be6ed4343b4
shrikant <shrikantvakna@gmail.com>
2026-04-04 13:32:31 +0530

    simple program to count the no. of the words inside the string

---
+++ string/counting_word/p1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char A[] = "How are you";
+
+	int i, word = 1;
+
+	for(i = 0; A[i]!='\0';i++)
+	{
+		if(A[i] == ' ' && A[i-1]!= ' ')
+		word++;
+	}
+	cout<<"No. of words are = "<<word<<endl;
+	return 0;
+}
